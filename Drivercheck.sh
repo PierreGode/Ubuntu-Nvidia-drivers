d54b99f6979df20bea0303c15faab2336f11b858
PierreGoude <pierre@webbhatt.com>
2017-03-02 11:04:40 +0100

    Update Drivercheck.sh

--- Drivercheck.sh
+++ Drivercheck.sh
@@ -11,40 +11,22 @@
 clear
 hardware=$(lspci | grep -i nvidia | cut -d '[' -f2 | cut -d ']' -f1)
 echo "${NUMBER}Hardware installed: $hardware ${END}"
-
-
-#if [ "$hardware" = "GeForce GTX 960M" ]
-#then
-#echo "OK"
-#else
-#echo "FAIL"
-#fi
+test=$(nvidia-smi)
+if [ $? = 127 ]
+then
+echo "${RED_TEXT}No driver seems to be installed ${END}"
+else
 driver=$(nvidia-smi | grep -i driver | cut -d ':' -f2 | cut -d '|' -f1 | sed -e 's/^[[:space:]]*//')
 echo "${NUMBER}Driver installed: $driver ${END}"
-apt=$(apt-cache search $driver)
+fi
+
+cat list.txt | while read output
+do
+if [ "$hardware" = "$output" ]
+then
+echo "The suggested driver for $hardware is $output"
+apt=$(apt-cache search $newdriver)
 echo "suggesting driver: $apt"
-list(){
-378.13=Quadro P3000
-378.13=Quadro GP100
-378.13=Quadro P4000
-378.13=Quadro P2000
-378.13=Quadro P1000
-378.13=Quadro P600
-378.13=Quadro P400
-378.13=Quadro M1200
-378.13=Quadro M2200
-340.102=xorg-server 1.19
-375.26=Quadro M3000 SE
-375.20=GeForce GTX 1050
-375.20=GeForce GTX 1050 Ti
-375.20=Quadro M620
-375.20=Quadro M5000 SE
-375.10=Quadro P6000
-375.10=Quadro P5000
-367.44=TITAN X
-367.44=TITAN X Pascal
-367.44=GeForce GTX 1060
-367.27=GeForce GTX 1080
-367.27=GeForce GTX 1070
-361.45.11=Quadro M5500
-}
+else
+echo "Fail"
+fi
