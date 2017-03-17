76356222cab404ec8eb3fa4f20a8d8acd6f48449
PierreGoude <pierre@webbhatt.com>
2017-03-17 13:59:44 +0100

    Update Drivercheck.sh

--- Drivercheck.sh
+++ Drivercheck.sh
@@ -1,11 +1,11 @@
-
+#!/bin/bash
 # ~~~~~~~~~~  Environment Setup ~~~~~~~~~~ #
-    NORMAL=`echo "\033[m"`
-    MENU=`echo "\033[36m"` #Blue
-    NUMBER=`echo "\033[33m"` #yellow
-    RED_TEXT=`echo "\033[31m"` #Red
-    INTRO_TEXT=`echo "\033[32m"` #green and white text
-    END=`echo "\033[0m"`
+    NORMAL=$(echo "\033[m")
+    MENU=$(echo "\033[36m") #Blue
+    NUMBER=$(echo "\033[33m") #yellow
+    RED_TEXT=$(echo "\033[31m") #Red
+    INTRO_TEXT=$(echo "\033[32m") #green and white text
+    END=$(echo "\033[0m")
 # ~~~~~~~~~~  Environment Setup ~~~~~~~~~~ #
 
 
